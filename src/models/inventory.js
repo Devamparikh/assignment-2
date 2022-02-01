a37023dca42f54f8a44f91dddfded4db12d6e97b
Devamparikh <devamparikh15@gmail.com>
2022-02-01 16:52:35 +0530

    refectored code

--- src/models/inventory.js
+++ src/models/inventory.js
@@ -15,11 +15,10 @@ const schemaCreateInventory = {
     expiryTime: {
         type: "date", 
         optional: true,
-        convert: true,
-        default: '0000-00-00 00:00:00'
+        convert: true
 
     },
-    quantity: { type: "number", default: 1, convert: true},
+    quantity: { type: "number", convert: true},
     manufacturingTime: { 
         type: "date",
         convert: true
@@ -45,13 +44,14 @@ const insertQuery = async (data, file) => {
         throw new Error("Wrong dates!")
     }
 
-    const [rows, fields] = await promisePool.execute("INSERT INTO `inventory`(`inventory_name`, `inventory_category`, `expiry_time`, `inventory_quantity`, `manufacturing_time`, `inventory_image`, `inventory_user_id`) VALUES (?,?,?,?,?,?,?)", [data.inventoryName, data.inventoryCategory, data.expiryTime, data.quantity, data.manufacturingTime, file.filename, 0])
+    const [rows, fields] = await promisePool.execute("INSERT INTO `inventory`(`inventory_name`, `inventory_category`, `expiry_time`, `inventory_quantity`, `manufacturing_time`, `inventory_image`) VALUES (?,?,?,?,?,?)", [data.inventoryName, data.inventoryCategory, data.expiryTime, data.quantity, data.manufacturingTime, file.filename])
     if (rows.length == 0){
         logger.warn('no such data in database insert query executed')
-        return 'no such data in database.'
+        return {error: 'no such data in database.', ok: false}
     }
+    console.log(rows)
     logger.info('insert query executed successfully: ' + rows )
-    return rows
+    return {id: rows.insertId, massage: 'insert query executed successfully', ok: true}
 
 }
 
@@ -62,25 +62,26 @@ const searchQuery = async (search, timezone) => {
     logger.info('data arrived at searchQuery function: ', search, timezone )
 
 
-    const [rows, fields] = await promisePool.execute("SELECT `inventory_id`, `inventory_name`, `inventory_category`, `expiry_time`, `inventory_quantity`, `inventory_image` FROM `inventory` WHERE `inventory_name` LIKE ? OR `inventory_category` LIKE ?", [search, search])
+    const [rows, fields] = await promisePool.execute("SELECT `inventory_id`, `inventory_name`, `inventory_category`, `expiry_time`, `inventory_quantity`, `inventory_image` FROM `inventory` WHERE `is_deleted` = 0 AND (`inventory_name` LIKE ? OR `inventory_category` LIKE ?)", [search, search])
     if (rows.length == 0){
         logger.warn('no such data in database search query executed')
-        return 'no such data in database.'
+        return {error: 'no such data in database.', ok: false}
     }
     logger.info('search query executed successfully: ' + rows )
 
     rows.forEach((row) => {
-        if((row.expiry_time == '0000-00-00 00:00:00') || (row.expiry_time > new Date()) ) {
+        if((row.expiry_time == '0000-00-00 00:00:00') || (moment.tz(row.expiry_time, timezone).format() > moment.tz(new Date(), timezone).format()) ) {
             row.is_exprired = false
         }else {
             row.is_exprired = true
         }
 
-        row.inventory_image = 'Public/data/uploads/' + row.inventory_image
-        row.expiryTime = moment.tz(row.expiryTime, timezone).format()
+        row.inventory_image = 'D:/Programming/Projects/marutiTechlabAssignment/marutiTechlabAssignment/assignment-2/Public/data/uploads/' + row.inventory_image
+        row.expiry_time = moment.tz(row.expiry_time, timezone).format()
+        // console.log(row.expiry_time)
 
     })
-
+    console.log(rows)
     return rows
 
 }
@@ -88,28 +89,16 @@ const searchQuery = async (search, timezone) => {
 
 
 //update query 
-const updateQuery = async (id, data, file) => {
-    logger.info('data arrived at updateQuery function: ', id, data, file )
-    // convert to CST time
-    data.manufacturingTime = moment.tz(data.manufacturingTime, "America/Chicago").format()
-    data.expiryTime = moment.tz(data.expiryTime, "America/Chicago").format()
+const updateQuery = async (id, quantity) => {
+    logger.info('data arrived at updateQuery function: ', id )
 
-
-    //check whether exp date is then mnf date
-    if((data.expiryTime !== '0000-00-00 00:00:00') && (data.expiryTime < data.manufacturingTime)){
-        // console.log("ERROR IN DATE")
-        logger.warn('ERROR IN DATE')
-        throw new Error("Wrong dates!")
-    }
-
-
-    const [rows, fields] = await promisePool.execute("UPDATE `inventory` SET `inventory_name`=?,`inventory_category`=?,`expiry_time`=?,`inventory_quantity`=?,`manufacturing_time`=?,`inventory_image`=? WHERE `inventory_id`=?", [data.inventoryName, data.inventoryCategory, data.expiryTime, data.quantity, data.manufacturingTime, file.filename, id])
+    const [rows, fields] = await promisePool.execute("UPDATE `inventory` SET `inventory_quantity`=? WHERE`is_deleted` = 0 AND  `inventory_id`=?", [ quantity, id])
     if (rows.length == 0){
         logger.warn('no such data in database update query executed')
-        return 'no such data in database.'
+        return {error: 'no such data in database.', ok: false}
     }
     logger.info('update query executed successfully: ' + rows )
-    return rows
+    return {massage: 'quantity updated successfully', ok: true}
 
 }
 
@@ -120,10 +109,10 @@ const deleteQuery = async (search) => {
 
     logger.info('data arrived at updateQuery function: ', search )
 
-    const [rows, fields] = await promisePool.execute("SELECT * FROM `inventory` WHERE `inventory_name` LIKE ? OR `inventory_category` LIKE ?", [search, search])
+    const [rows, fields] = await promisePool.execute("SELECT * FROM `inventory` WHERE `is_deleted` = 0 AND (`inventory_name` LIKE ? OR `inventory_category` LIKE ?)", [search, search])
     if (rows.length == 0){
         logger.warn('no such data in database delete query executed')
-        return 'no such data in database.'
+        return {error: 'no such data in database.', ok: false}
     }
     rows.forEach(async(row) =>{
 
@@ -138,21 +127,17 @@ const deleteQuery = async (search) => {
         }
         
         
-        const [deletestatus, fields] = await promisePool.execute("UPDATE `inventory` SET `is_deleted`= ?, `inventory_image`=? WHERE `inventory_id` = ?", [ 'true' , null, row.inventory_id])
-        if (deletestatus.length == 0){
-        logger.warn('no such data in database delete query executed')
-        return 'no such data in database.'
-        }
+        
 
         logger.info('successfully data deleted for id: ', row.inventory_id )
     })
-    logger.info('delete query executed successfully')
-    const [currentrows] = await promisePool.execute("SELECT * FROM `inventory` WHERE `inventory_name` LIKE ? OR `inventory_category` LIKE ?", [search, search])
-    if (rows.length == 0){
+    const [deleteStatus] = await promisePool.execute("UPDATE `inventory` SET `is_deleted`= ?, `inventory_image`=? WHERE `is_deleted` = 0 AND (`inventory_name` LIKE ? OR `inventory_category` LIKE ?)", [ 1 , NULL, search, search])
+    if (deleteStatus.length == 0){
         logger.warn('no such data in database delete query executed')
-        return 'no such data in database.'
+        return {error: 'no such data in database.', ok: false}
     }
-    return currentrows
+    logger.info('delete query executed successfully')
+    return {massage: 'delete query executed successfully', ok: true}
 
 }
 
--- src/routers/inventory.js
+++ src/routers/inventory.js
@@ -3,6 +3,7 @@ const multer  = require('multer')
 const router = new express.Router()
 const logger = require('../logger/logger')
 const { check, insertQuery, searchQuery, updateQuery, deleteQuery } = require('../models/inventory')
+const validate = require('../middleware/validate')
 
 
 // add extention to file name
@@ -22,8 +23,13 @@ const storage = multer.diskStorage({
 const upload = multer({
     storage: storage,
     fileFilter(req, file, cb){
+        // console.log("req.body form upload function: " , req)
+        // console.log(req.manufacturingTime)
+        // console.log(req.inventoryCategory)
+
+        // console.log("file form upload function: " , file)
         if(!file.originalname.match(/\.(png|jpeg|jpg)$/)){
-            return cb(new Error('Please upload an Image.'))
+            return cb(new Error('invalid file type'))
         }
         // console.log(req, file)
         cb(undefined, true)
@@ -33,15 +39,24 @@ const upload = multer({
 
 
 // insert data router
-router.post('/insertinventory', upload.single('uploaded_file'), async (req, res) => {
+router.post('/inventory', upload.single('uploadedFile'), async (req, res) => {
     try {
+        // console.log(unknowIdentify)
         logger.info('insert data route start.')
+        // console.log("req.body form post function: ",req.body)
+        // console.log(req.file, req.body)
+
         const valid = await check(req.body)
+        if(!req.file.filename){
+            logger.error('invalid data after check sending response improper data.')
+            return res.status(400).send({error: 'Improper data.', message: 'filename required', ok: false})
+        }
+        console.log("valid: " , valid)
         logger.info('valid data check: ' , valid)
         // console.log(valid)
         if (valid !== true) {
             logger.error('invalid data after check sending response improper data.')
-            return res.status(400).send({error: 'Improper data.'})
+            return res.status(400).send({error: 'Improper data.', message: valid.message, ok: false})
         }
 
         logger.info('sending req.body and req.file in insertQuery function')
@@ -53,7 +68,6 @@ router.post('/insertinventory', upload.single('uploaded_file'), async (req, res)
 
 
 
-        console.log(req.file, req.body)
 
         // const currentDate = new Date()
         // console.log(currentDate)
@@ -62,9 +76,9 @@ router.post('/insertinventory', upload.single('uploaded_file'), async (req, res)
 
         
     } catch (error) {
-        console.log(error)
+        // console.log(error)
         logger.error('error in insert router: ', error)
-        res.status(500).send(error)
+        res.status(500).send({error: error.message, ok: false})
     }
     
     
@@ -72,7 +86,7 @@ router.post('/insertinventory', upload.single('uploaded_file'), async (req, res)
 
 
 // search data router
-router.get('/searchinventory/:search', async (req, res) => {
+router.get('/inventory/:search', async (req, res) => {
     try {
         logger.info('search data route start.')
         
@@ -100,7 +114,7 @@ router.get('/searchinventory/:search', async (req, res) => {
     } catch (error) {
         // console.log(error)
         logger.error('error in search router: ', error)
-        res.status(500).send(error)
+        res.status(500).send({error: error.message, ok: false})
     }
     
     
@@ -108,33 +122,32 @@ router.get('/searchinventory/:search', async (req, res) => {
 
 
 //update data router
-router.put('/updateinventory/:id', upload.single('uploaded_file'), async (req, res) => {
+router.patch('/inventory', async (req, res) => {
     try {
         logger.info('update data route start.')
         
-        const id = req.params.id
+        const id = req.body.id
+        const quantity = req.body.quantity
 
-        
-        const valid = await check(req.body)
-        logger.info('valid data check: ' , valid)
-        if (valid !== true) {
+        if(!quantity || !id){
             logger.error('invalid data after check sending response improper data.')
-            return res.status(400).send({error: 'Improper data.'})
+            return res.status(400).send({error: 'quantity or id not specified!', ok: false})
         }
 
+
         logger.info('sending id, req.body and req.file in updateQuery function')
-        const rows = await updateQuery(id, req.body, req.file)
+        const rows = await updateQuery(id, quantity)
         // console.log("rows in router: ", rows)
         logger.info('response from updateQuery function in router file: '+ rows)
         logger.info('sending response from router using send')
 
-        res.send(rows)
+        res.status(200).send({massage: 'quantity updated successfully!', ok: true})
 
         
     } catch (error) {
         // console.log(error)
         logger.error('error in update router: ', error)
-        res.status(500).send(error)
+        res.status(500).send({error: error.message, error: error, ok: false})
     }
     
     
@@ -143,7 +156,7 @@ router.put('/updateinventory/:id', upload.single('uploaded_file'), async (req, r
 
 
 //delete data router
-router.delete('/deleteinventory/:search', async (req, res) => {
+router.delete('/inventory/:search', async (req, res) => {
     try {
         logger.info('delete data route start.')
         
@@ -161,7 +174,7 @@ router.delete('/deleteinventory/:search', async (req, res) => {
     } catch (error) {
         // console.log(error)
         logger.error('error in delete router: ', error)
-        res.status(500).send(error)
+        res.status(500).send({error: error.message, ok: false})
     }
     
     
